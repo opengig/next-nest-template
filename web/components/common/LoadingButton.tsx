b7b2652dcb7343c402847e1d41a323c42bf1c561
Nilesh <nilesh.28.2003@gmail.com>
2025-07-06 17:16:43 +0530

    refactor: remove unused import in LoadingButton component for cleaner code

--- web/components/common/LoadingButton.tsx
+++ web/components/common/LoadingButton.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Button, ButtonProps } from '@/components/ui/button';
 import { Loader2 } from 'lucide-react';
-import { cn } from '@/lib/utils';
 
 interface LoadingButtonProps extends ButtonProps {
   loading?: boolean;
